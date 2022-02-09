2bb4c0a8a40fe84c5edb9abe6385be2df964a15d
Sarah Roberts <sarahr@cyverse.org>
2022-02-09 16:41:08 -0700

    CORE-1678: added a duplicate check to the POST /v1/admin/resource-types endpoint

--- internal/controllers/admin_resource_types.go
+++ internal/controllers/admin_resource_types.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/cyverse/QMS/internal/model"
 	"github.com/labstack/echo/v4"
+	"gorm.io/gorm/clause"
 )
 
 // AddResourceType is the handler for the POST /v1/admin/resource-types endpoint.
@@ -33,11 +34,23 @@ func (s Server) AddResourceType(ctx echo.Context) error {
 		return model.Error(ctx, msg, http.StatusBadRequest)
 	}
 
+	// Guard against the case where the ID is specified in the request body, which breaks our duplicate check.
+	resourceType.ID = nil
+
 	// Save the resource type.
-	result := s.GORMDB.Select("ID", "Name", "Unit").Create(&resourceType)
+	result := s.GORMDB.
+		Select("ID", "Name", "Unit").
+		Clauses(clause.OnConflict{DoNothing: true}).
+		Create(&resourceType)
 	if result.Error != nil {
 		msg := fmt.Sprintf("unable to save the resource type: %s", result.Error)
 		return model.Error(ctx, msg, http.StatusInternalServerError)
+	}
+
+	// If the ID wasn't populated and an error didn't occur then there must have been a conflict.
+	if resourceType.ID == nil || *resourceType.ID == "" {
+		msg := fmt.Sprintf("a resource type with the given name already exists: %s", resourceType.Name)
+		return model.Error(ctx, msg, http.StatusConflict)
 	}
 
 	return model.Success(ctx, resourceType, http.StatusOK)
